b16762820307fdf826f61332d846d9b2436668c2
Elvinp329 <elvinkj@code.edu.az>
2023-01-02 22:17:22 +0400

    taskfour

---
+++ task 1/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            int r1, r2;
+            int tmp;
+            bool isDifferencedigit = true;
+            while (number != 0)
+            {
+
+                r1 = number % 10;
+                number /= 10;
+                tmp = number;
+
+                while (tmp != 0)
+                {
+                    r2= tmp % 10;
+                    tmp /= 10;
+
+                    if (r1 == r2)
+                    {
+                        Console.WriteLine("Muxtelif reqemli deyil");
+                        isDifferencedigit = false;
+                        break;
+                    }
+
+
+                }
+
+                if (!isDifferencedigit)
+                    break;
+            }
+                                                          
+                            
+            if (!isDifferencedigit ) 
+            Console.WriteLine("Muxtelif Reqemlidir");
+        }
+    }
+}
